2468b2de532385474e3de54def0db6a667b79a02
params19 <paramsrivastava9@gmail.com>
2024-06-26 18:35:36 +0530

    feat: Add smooth_page_indicator package for page indicators

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,18 +4,19 @@ import 'package:news_app/models/category_models.dart';
 import 'package:news_app/services/data.dart';
 import 'package:news_app/models/slider_model.dart';
 import 'package:news_app/services/slider_data.dart';
- 
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
+  
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   List<CategoryModel> categories = [];
-  List<SliderModel> sliders=[];
+  List<SliderModel> sliders = [];
+  int activeIndex = 0;
 
   @override
   void initState() {
@@ -25,119 +26,183 @@ class _HomeState extends State<Home> {
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Flutter"),
-          Text(
-            "News",
-            style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-          )
-        ],
-      ),
-      centerTitle: true,
-      elevation: 0.0,
-    ),
-    body: Container(
-      child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(left: 10),
-            height: 70,
-            child: ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return CategoryTile(
-                  image: categories[index].image,
-                  categoryName: categories[index].categoryName,
-                );
-              },
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Flutter"),
+            Text(
+              "News",
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
             ),
-          ),
-
-          SizedBox(height: 30,),
-
-          CarouselSlider(
-            items: sliders.map((slider) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return buildImage(slider.image!, sliders.indexOf(slider), slider.name!);
+          ],
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 10),
+              height: 70,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return CategoryTile(
+                    image: categories[index].image,
+                    categoryName: categories[index].categoryName,
+                  );
                 },
-              );
-            }).toList(),
-            options: CarouselOptions(
-              height: 250,
-              // viewportFraction: 1,
-              autoPlay: true,
-              enlargeCenterPage: true ,
-              enlargeStrategy: CenterPageEnlargeStrategy.height,
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Breaking News!",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  Text(
+                    "View All",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            CarouselSlider.builder(
+              itemCount: sliders.length,
+              itemBuilder: (context, index, realIndex) {
+                String? res = sliders[index].image;
+                String? res1 = sliders[index].name;
+                return buildImage(res!, index, res1!);
+              },
+              options: CarouselOptions(
+                height: 250,
+                autoPlay: true,
+                enlargeCenterPage: true,
+                enlargeStrategy: CenterPageEnlargeStrategy.height,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    activeIndex = index;
+                  });
+                },
+              ),
+            ),
+            const SizedBox(height: 30),
+            buildIndicator(),
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-  Widget buildImage(String image,int index, String name)=>Container(
+  Widget buildImage(String image, int index, String name) => Container(
     margin: const EdgeInsets.symmetric(horizontal: 5),
     child: Stack(
       children: [
         ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: Image.asset(image,height: 250, fit:BoxFit.cover, width: MediaQuery.of(context).size.width,)
-      ),
-      Container(
-        height: 250,
-        padding: const EdgeInsets.only(left: 10),
-        margin: const EdgeInsets.only(top: 170),
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.black26,
-          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10))
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            image,
+            height: 250,
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+          ),
         ),
-        child: Text(name,style: TextStyle(color: Colors.white,fontSize: 20.0,fontWeight: FontWeight.bold),)
-      )
-  ]));
+        Container(
+          height: 250,
+          padding: const EdgeInsets.only(left: 10),
+          margin: const EdgeInsets.only(top: 170),
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            color: Colors.black26,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+          child: Text(
+            name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  Widget buildIndicator() => AnimatedSmoothIndicator(
+    activeIndex: activeIndex,
+    count: sliders.length,
+    effect: SlideEffect(
+      dotWidth: 15,
+      dotHeight: 15,
+      activeDotColor: Colors.blue,
+    ),
+  );
 }
 
 class CategoryTile extends StatelessWidget {
- final image, categoryName;
- CategoryTile({this.image, this.categoryName});
+  final image, categoryName;
+
+  CategoryTile({this.image, this.categoryName});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(right: 16),
-      child: Stack(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(6),
-          child: Image.asset(image, width: 120, height: 70, fit: BoxFit.cover,)),
-        Container(
-  width: 120, 
-  height: 70,
-  decoration: BoxDecoration(
-    color: Colors.black38, // Specify color here
-    borderRadius: BorderRadius.circular(6), // And other decoration properties
-  ),
-  child: Center(
-    child: Text(
-      categoryName, 
-      style: const TextStyle(
-        color: Colors.white, 
-        fontSize: 15,
-        fontWeight: FontWeight.bold
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.asset(
+              image,
+              width: 120,
+              height: 70,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: 120,
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.black38,
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Center(
+              child: Text(
+                categoryName,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-    ),
-  ),
-)
-
-      ],)
     );
   }
 }
