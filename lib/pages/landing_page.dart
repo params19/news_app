6132d2cc4582c4b84c332342b75402372fc02850
params19 <paramsrivastava9@gmail.com>
2024-06-29 11:58:24 +0530

    feat: Improve landing page layout and add trending news section

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -12,14 +12,74 @@ class LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: Column(
           children :[
-            Image.asset(
-              "assets/image.png",
-              width: MediaQuery.of(context).size.width,
-              height: 400,
-              fit: BoxFit.cover,
+            Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(30),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child:
+                  Image.asset(
+                    "assets/image.png",
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/2,
+                    fit: BoxFit.cover,
+                  ),
+              ),
             ),
+            const SizedBox(height: 20),
+            const Center(
+              child: Text(
+                "News from around the world",
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Center(
+              child: Text(
+                "Read the latest news from around the world",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black45,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 40),
+
+            Container(
+              width: MediaQuery.of(context).size.width/1.2,
+              child: Material(
+                borderRadius: BorderRadius.circular(30),
+                elevation: 5,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 15),  
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: const Center(
+                    child:  Text(
+                      "Get Started",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ), 
           ],
         ),
       ),
